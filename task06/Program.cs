b786d2f76d91c852e3d7500e117482716f87c831
Korgi <korgi_korgi@rambler.ru>
2022-12-20 13:56:31 +0300

    Решили задачу 6

---
+++ task06/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая
+// 1 на вход принимает число
+// 2 и выдает, является ли число четным
+
+Console.Write("Привет! Введи целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number % 2 == 0) Console.WriteLine("Число четное?: да");
+else Console.WriteLine("Число четное?: нет");

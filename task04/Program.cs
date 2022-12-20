116d31285e5c47289c6a4bab13b4ae46666523e4
Korgi <korgi_korgi@rambler.ru>
2022-12-20 13:36:04 +0300

    Выполнили задачу 4

---
+++ task04/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая
+// 1 принимает на вход 3 числа
+// 2 и выдает максимальное из этих чисел
+
+Console.WriteLine("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+int max = firstNumber;
+if (firstNumber > max) max = firstNumber;
+if (secondNumber > max) max = secondNumber;
+if (thirdNumber > max) max = thirdNumber;
+Console.Write("max = ");
+Console.Write(max);

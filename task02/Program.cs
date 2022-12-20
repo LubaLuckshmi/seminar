0d956f9e6830f8607538e05d1dbe29a0385dfe2b
Korgi <korgi_korgi@rambler.ru>
2022-12-20 13:19:33 +0300

    Выполнили задачу 2

--- task02/Program.cs
+++ task02/Program.cs
@@ -2,4 +2,16 @@
 1 на вход принимает 2 числа
 2 и выдает, какое число большее, а какое меньшее */
 
-
+Console.WriteLine("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+int max = firstNumber;
+if(secondNumber > max) 
+{
+Console.WriteLine($"Max = {secondNumber}");
+}
+else 
+{
+Console.WriteLine($"Max = {firstNumber}");
+}

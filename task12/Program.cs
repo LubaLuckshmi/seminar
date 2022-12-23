2a6ccd131d0c5c85c7fd067d5b8826cc7528e232
Korgi <korgi_korgi@rambler.ru>
2022-12-23 20:21:40 +0300

    Добавили метод и тернарный оператор в задачу 12

--- task12/Program.cs
+++ task12/Program.cs
@@ -8,6 +8,14 @@
 Console.WriteLine("Введите 2 числа: ");
 int firstNumber = Convert.ToInt32(Console.ReadLine());
 int secondNumber = Convert.ToInt32(Console.ReadLine());
-bool result = firstNumber % secondNumber == 0;
-if (result) Console.WriteLine("Кратно");
-else Console.WriteLine($"Не кратно, остаток {firstNumber % secondNumber}");
+int res = Multiplicity(firstNumber, secondNumber);
+Console.WriteLine(res == 0 ? "Кратно" : $"Не кратно, остаток от деления {res}");
+
+// bool result = firstNumber % secondNumber == 0;
+// if (result) Console.WriteLine("Кратно");
+// else Console.WriteLine($"Не кратно, остаток {firstNumber % secondNumber}");
+
+int Multiplicity(int firstNumber, int secondNumber)
+{
+    return firstNumber % secondNumber;
+}

889e91d5799da9e178099b17ee518487df51fba9
Korgi <korgi_korgi@rambler.ru>
2022-12-24 01:26:42 +0300

    Решили задачу 13 с терн опер и одной ошибкой

---
+++ task13/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая 
+// 2 выводит третью цифру 2 заданного числа
+//или сообщает, что третьей цифры нет
+//  645 -> 5, 78 -> третьей цифры нет, 32679 -> 6
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int thirdDigit = number %10;
+
+Console.WriteLine(number >= 100 ?
+ $"Третья цифра в числе {number} равна {thirdDigit}" : "Третьей цифры нет");

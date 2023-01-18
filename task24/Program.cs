8b9fdecd0a3a6d1db5f6c6b61288853f0bdcab86
us1990 <korgi_korgi@rambler.ru>
2023-01-18 20:16:05 +0300

    Решили задачу 24

--- task24/Program.cs
+++ task24/Program.cs
@@ -8,4 +8,18 @@
 Console.WriteLine("Введите целое число");
 int number = Convert.ToInt32(Console.ReadLine());
 // int number1 = int.Parse(Console.ReadLine());
+int sumNumbers = SumNumbers(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} = {sumNumbers}");
 
+
+int SumNumbers(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        // sum = sum +1
+        sum+=i;
+    }
+return sum;
+
+}

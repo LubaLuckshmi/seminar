3d9869865f60251c239118cfc9941dcbdd0ce1c5
us1990 <korgi_korgi@rambler.ru>
2023-01-18 19:04:08 +0300

    добавили задачу 24

---
+++ task24/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 24: Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+// int number1 = int.Parse(Console.ReadLine());
+
